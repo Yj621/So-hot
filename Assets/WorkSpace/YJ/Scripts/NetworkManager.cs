3bec0e446a76e3838d04b98d49bbcf3412db8bca
Yj621 <87031282+Yj621@users.noreply.github.com>
2025-02-20 14:08:11 +0900

    [FEAT] NetworkManager and StartBoard Animation
    로비 입장시와 Start Board 애니메이션 추가

--- Assets/WorkSpace/YJ/Scripts/NetworkManager.cs
+++ Assets/WorkSpace/YJ/Scripts/NetworkManager.cs
@@ -1,16 +1,47 @@
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private static string userNickName;
+    private const int MaxNicknameLenght = 8;
+
+    [Header("--- Panel ---")]
+    public GameObject startUI;
+    public GameObject nickNameUI;
+    public GameObject roomListUI;
+    public GameObject createRoomUI;
+    public GameObject settingUI;
+    public GameObject QuitUI;
+
+    [Header("--- Text ---")]
+
+    [Header("--- InputField ---")]
+    public TMP_InputField nickNameInput;
+    public TMP_InputField roomNameInput;
+
+    [Header("--- Room List ---")]
+    public GameObject roomListContent;
+
+
     void Start()
     {
-        
+        //포톤 연결 설정
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    //마스터 서버 연결시
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
     }
+
+    //로비에 성공적으로 입장하면 호출되는 메서드 (StartPanel이 나타남)
+    public override void OnJoinedLobby()
+    {
+        startUI.SetActive(true);
+    }
+
 }
