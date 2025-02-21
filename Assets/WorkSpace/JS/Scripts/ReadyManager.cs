9bff4f96598a8aea33e9b756d24d1696b70b6668
hipop1109-commits <hipop1109@gmail.com>
2025-02-21 17:55:33 +0900

    [FEAT] CharacterPick Update
    캐릭터 선택 정상화

--- Assets/WorkSpace/JS/Scripts/ReadyManager.cs
+++ Assets/WorkSpace/JS/Scripts/ReadyManager.cs
@@ -2,115 +2,94 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
-using System.Collections.Generic;
 
-public class ReadyManager : MonoBehaviour
+public class ReadyManager : MonoBehaviourPunCallbacks
 {
-    public GameObject[] characters; //캐릭터 변환
-    public Sprite[] characterImages; //캐릭터 이미지 모음
-    public Image characterImage; //이미지를 꽂을 곳
-    private int currentIndex = -1; //현재 인덱스
-    public Sprite unknownCharacterSprite; // ??? 이미지 (선택 해제 상태)
-    public GameObject blackCharacter; // 선택되지 않은 상태에서 보여줄 검은색 캐릭터
+    public Text playerNameText;
+    public GameObject[] characters;
+    public Sprite[] characterImages;
+    public Image characterImage;
+    public Sprite unknownCharacterSprite;
+    public GameObject blackCharacter;
+
+    private int currentIndex = -1;
     private GameObject currentCharacter;
+    public int playerSlot = -1; // 플레이어 슬롯 (1P~4P)
 
-    private Dictionary<int, int> playerSelections = new Dictionary<int, int>();
-
-    private PhotonView pv;
-
-    public GameObject[] characterPrefabs; // 캐릭터 프리팹 목록
-    public Transform[] spawnPoints;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        pv = GetComponent<PhotonView>();
-        ResetSelection(); //들어갈 때 리셋하고 시작
-    }
+        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("PlayerSlot"))
+        {
+            playerSlot = (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerSlot"];
+            Debug.Log($"내 슬롯 번호: {playerSlot + 1}P");
+        }
+        else
+        {
+            Debug.LogError("플레이어 슬롯 정보를 찾을 수 없습니다.");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ResetSelection();
     }
 
     public void OnLeftArrow()
     {
-        if (!pv.IsMine) return;
+        if (!CanControl()) return;
 
-        currentIndex = (currentIndex -1 + characters.Length) % characters.Length;
-        UpdateCharacterDisplay(); //캐릭터 순서 정렬하고 업뎃
+        currentIndex = (currentIndex - 1 + characters.Length) % characters.Length;
+        UpdateCharacterDisplay();
     }
 
     public void OnRightArrow()
     {
-        if (!pv.IsMine) return;
+        if (!CanControl()) return;
 
         currentIndex = (currentIndex + 1) % characters.Length;
-        UpdateCharacterDisplay(); //똑같이 업뎃
-
-        pv.RPC("RPCUpdateCharacterSelection", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, currentIndex);
+        UpdateCharacterDisplay();
     }
 
     public void OnSelect()
     {
-        if (currentIndex == -1) return; // 아무 캐릭터도 선택 안 되어 있을 때 예외 처리
-        Debug.Log("선택된 캐릭터: " + characters[currentIndex].name);
-        pv.RPC("RPCConfirmCharacterSelection", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, currentIndex);
+        if (!CanControl() || currentIndex == -1) return;
+
+        Debug.Log($"{playerSlot + 1}P가 캐릭터 {characters[currentIndex].name}을(를) 선택함.");
     }
 
     public void OnCancel()
     {
-        ResetSelection(); //취소하면 다시 검은색으로 돌아감
+        if (!CanControl()) return;
+
+        ResetSelection();
     }
 
     private void UpdateCharacterDisplay()
     {
-        // 이전 캐릭터 비활성화
         blackCharacter.SetActive(false);
+
         if (currentCharacter != null)
         {
             currentCharacter.SetActive(false);
         }
 
-        // 새로운 캐릭터 활성화
         currentCharacter = characters[currentIndex];
         currentCharacter.SetActive(true);
 
-        // 초상화 업데이트
         characterImage.sprite = characterImages[currentIndex];
     }
-
 
     private void ResetSelection()
     {
         if (currentCharacter != null)
         {
-            currentCharacter.SetActive(false);//남은 거 다 지우고
-        } 
-        blackCharacter.SetActive(true); //검은캐릭터 올리기
-        currentIndex = -1; //인덱스는 다시 설정 불가한걸로
-        characterImage.sprite = unknownCharacterSprite; //캐릭터 이미지 불명이미지
+            currentCharacter.SetActive(false);
+        }
+
+        blackCharacter.SetActive(true);
+        currentIndex = -1;
+        characterImage.sprite = unknownCharacterSprite;
     }
 
-    [PunRPC]
-    private void RPCUpdateCharacterSelection(int actorNumber, int selectedIndex)
+    private bool CanControl()
     {
-        if (playerSelections.ContainsKey(actorNumber))
-        {
-            playerSelections[actorNumber] = selectedIndex; 
-            //actorIndex 번호의 캐릭터 선택은 selectedIndex 번의 캐릭터이다
-        }
-        else
-        {
-            playerSelections.Add(actorNumber, selectedIndex);
-            //키를 가지지 않았다면 저걸 추기해준다
-        }
-    }
-
-    [PunRPC]
-    private void RPCConfirmSelections(int actorNumber, int selectdIndex)
-    {
-
+        return PhotonNetwork.LocalPlayer.ActorNumber == playerSlot;
     }
 }
