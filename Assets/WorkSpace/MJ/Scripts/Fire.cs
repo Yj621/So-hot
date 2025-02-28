56a3e9953a67d97ff7ddd2943977d411c39cccba
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-28 17:56:15 +0900

    [FEAT] Fire Timer Implemented
    불 떨어뜨리면 5초 뒤 꺼지고 세이브 포인트로 회귀하는 기능 구현현

--- Assets/WorkSpace/MJ/Scripts/Fire.cs
+++ Assets/WorkSpace/MJ/Scripts/Fire.cs
@@ -5,6 +5,8 @@ public class Fire : MonoBehaviourPun
 {
     public static Fire Instance;
     public bool isOnFire; //불이 켜져있는지 확인하는 bool 값
+    public bool isOnGround; //불이 바닥에 떨어져있는지 여부 확인하는 bool 값
+    public float timer = 5f; //바닥에 떨어졌을 때의 isOnFire 유지 시간
     public Vector3 firstFirePos; //불이 처음 로드될 때의 position 값
 
     private void Awake()
@@ -25,18 +27,49 @@ public class Fire : MonoBehaviourPun
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Goal"))
+        if (other.CompareTag("Finish"))
         {
             GameManager.Instance.photonView.RPC("GameClear", RpcTarget.All);
         }
         else if (other.CompareTag("Water"))
         {
-            isOnFire = false;
-            GameManager.Instance.photonView.RPC("AllPlayerRespawn", RpcTarget.All);
-
+            FireOff();
         }
-        //TO-DO: 불이 바닥에 오래 떨어져있으면, 불이 꺼지고 All Player Respawn 되는 if문 추가
+        else if (other.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            timer = 5f;
+            isOnGround = false;
+        }
     }
 
-    
+    void FireOff()
+    {
+        //중복 호출 방지(불이 꺼져있다면, 메서드 동작 종료)
+        if (!isOnFire) return;
+
+        isOnFire = false;
+        GameManager.Instance.photonView.RPC("AllPlayerRespawn", RpcTarget.All);
+    }
+
+    private void Update()
+    {
+        if(isOnGround)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                FireOff();
+                timer = 5f;
+                isOnGround = false;
+            }
+
+        }
+    }
 }
