52038bfcbb59681dd278fd7aca2ba299e1be6e31
Yj621 <87031282+Yj621@users.noreply.github.com>
2025-03-07 08:59:15 +0900

    Draft Voice On/Off
    보이스 기능 초안

--- Assets/WorkSpace/YJ/Scripts/VoiceManager.cs
+++ Assets/WorkSpace/YJ/Scripts/VoiceManager.cs
@@ -1,6 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class VoiceManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI player1Text;
+    [SerializeField] private TextMeshProUGUI player2Text;
+    [SerializeField] private TextMeshProUGUI player3Text;
+    [SerializeField] private TextMeshProUGUI player4Text;
 
+    //PlayerPick(Clone)
 }
