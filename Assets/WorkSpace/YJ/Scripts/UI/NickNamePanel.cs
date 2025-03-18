07481b4769d3257f165e4a42037e1aed4202c58a
Yj621 <87031282+Yj621@users.noreply.github.com>
2025-03-18 17:56:53 +0900

    [FEAT] NickName Panel
    닉네임 띄우기

--- Assets/WorkSpace/YJ/Scripts/UI/NickNamePanel.cs
+++ Assets/WorkSpace/YJ/Scripts/UI/NickNamePanel.cs
@@ -1,8 +1,20 @@
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
-public class NickNamePanel : MonoBehaviour
+public class NickNamePanel : MonoBehaviourPunCallbacks
 {
-   
+    private TextMeshProUGUI nickNameText;
+    private void Start()
+    {
+        nickNameText = GetComponentInChildren<TextMeshProUGUI>();        
+        // 이 PhotonView가 본인 소유일 때만 닉네임을 업데이트합니다.
+        if (photonView.IsMine)
+        {
+            nickNameText.text = PhotonNetwork.LocalPlayer.NickName;
+        }
+    }
+
     void Update()
     {
         transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
